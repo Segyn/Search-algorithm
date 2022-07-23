58105cac3bdf0edc7919beb1266bed40b0768f0a
ak10s <ak10s@mail.ru>
2022-07-23 21:07:56 +0500

    алгоритм поиска пузырьковым методом не прошёл проверку

--- gameAlgoritm/fanction.py
+++ gameAlgoritm/fanction.py
@@ -5,7 +5,7 @@ def start_algorithm():
     search_algorithm(user_number, min_number, max_number)
 
 
-def search_algorithm(user_number, min_number, max_number):
+def search_algorithm(user_number: int, min_number: int, max_number: int):
     search = int((max_number - min_number) / 2)
     print(search)
     if search == user_number:
@@ -17,6 +17,7 @@ def search_algorithm(user_number, min_number, max_number):
     print(min_number)
     print(max_number)
     search_algorithm(user_number, min_number, max_number)
+
 
 if __name__ == '__main__':
     start_algorithm()
