c0ab873cee73427c1565b8b8e168caa4529448a9
ak10s <ak10s@mail.ru>
2022-07-23 22:07:11 +0500

    алгоритм работает полностью.

--- gameAlgoritm/fanction.py
+++ gameAlgoritm/fanction.py
@@ -1,4 +1,4 @@
-#алгоритм пузырькового метода
+# Алгоритм пузырькового метода
 
 def start_algorithm():
     user_number = int(input('Введите число то 0 до 100: '))
@@ -9,10 +9,10 @@ def start_algorithm():
 
 def search_algorithm(user_number: int, min_number: int, max_number: int):
     search = int((max_number - min_number) / 2)
-    if min_number +search== user_number == max_number-search:
+    if min_number == user_number or min_number + search == user_number or max_number - search == user_number or max_number == user_number:
         return print('Конец')
-    if min_number+search>user_number:
-        max_number-=search
+    if min_number + search > user_number:
+        max_number -= search
     else:
         min_number += search
 
